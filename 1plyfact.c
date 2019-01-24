1c399e752b0c1fcf310ee3524fe4f573ddb4f33a
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-01-24 15:51:49 +0530

    Update 1plyfact.c

--- 1plyfact.c
+++ 1plyfact.c
@@ -5,10 +5,17 @@ void main()
     int num,i,sum=1;
     printf("enter the num");
     scanf("%d",&num);
+    if(num<=20)
+    {
     for(i=num;i>0;i--)
     {
         sum=sum*i;
     }
     printf("the factorial of given num is%d",sum);
+    }
+    else
+    {
+        printf("given num is above 20");
+    }
     getch();
 }
