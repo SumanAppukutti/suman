d7494818011def2155819f25f37b451b4ff8a9f8
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-01-28 15:31:20 +0530

    Create 1hello7.c

---
+++ 1hello7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    printf("enter the num");
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        printf("hello\n");
+    }
+    getch();
+}
