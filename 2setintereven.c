51c80ab4ca54917d43e88700b2e4521fc4149664
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-08 06:53:47 -0800

    Create 2setintereven.c

---
+++ 2setintereven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+printf("enter the intervels");
+scanf("%d %d",&a,&b);
+for(i=a;i<=b;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+}
+getch();
+}
