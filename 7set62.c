9ef7a170213d6c0b39f05b073af4db8d0d5495dd
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:43:58 +0530

    Create 7set62.c

---
+++ 7set62.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+	scanf("%s",a);
+
+	int i,flag=0,m;
+	m=strlen(a);
+	for(i=0;i<m;i++)
+	{
+		if(a[i]=='1' || a[i]=='0')
+		{
+			flag=1;
+		}
+		else
+		{
+			flag=0;
+			break;
+		}
+		
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+  getch();	
+}
