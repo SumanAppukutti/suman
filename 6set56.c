3b79c86b73907a58073653e985246f53bbf2ce12
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:30:17 +0530

    Create 6set56.c

---
+++ 6set56.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdio.h>
+void main()
+{
+
+    char s1[50],s2[50],ch=',';
+    printf("enter the string :");
+    scanf("%[^\n]s",s1);
+    printf("%c %c %c",s1[0],ch,s1[2]);
+    getch();
+
+}
