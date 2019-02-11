9371997c7db5117f6692a88a0d77ae696534ba0d
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:20:18 +0530

    Create 6set52.c

---
+++ 6set52.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf("three");
+break;
+case 4:
+printf("four");
+break;
+case 5:
+printf("five");
+break;
+case 6:
+printf("six");
+break;
+case 7:
+printf("seven");
+break;
+case 8:
+printf("eight");
+break;
+case 9:
+printf("nine");
+break;
+case 0:
+printf("zero");
+break;
+default:
+printf("no such number");
+}
+getch();
+}
