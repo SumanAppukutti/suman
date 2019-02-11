6ae7711c0fe2937f7b65dacffa7bbf8fb6e213e1
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:57:40 +0530

    Create 5set42.c

---
+++ 5set42.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[30],str1[40];
+int i,j,flag=0;
+printf("enter 2 strings");
+gets(str);
+gets(str1);
+for(i=0;str[i]!='\0';i++)
+{
+for(j=0;str1[j]!='\0';j++)
+{
+if(str[i]==str1[j])
+{
+flag=0;
+}
+else
+{
+flag=1;
+}
+}
+}
+if(flag==0)
+{
+puts(str);
+}
+else
+{
+puts(str1);
+}
+getch();
+}
