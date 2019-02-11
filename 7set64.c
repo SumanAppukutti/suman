bf749a65f0b619ce757ca8882dc183d3f2283b08
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:48:19 +0530

    Create 7set64.c

---
+++ 7set64.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h> 
+void main() 
+{
+	int n1,n2,sum;
+	printf("enter the number1:");
+	scanf("%d",&n1);
+	printf("enter the number2:");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	if(sum%2==0)
+	{
+		printf("\neven");
+	}
+	else
+	{
+		printf("\nodd");
+	}
+	getch();
+}
