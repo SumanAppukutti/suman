f35487864e94f5c1be2ac753bafae0f926508073
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:01:59 +0530

    Create 5set44.c

---
+++ 5set44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#oinclude<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n>1)&&(n<10))
+{
+printf("%d",n);
+}
+else
+{
+    printf("not in range");
+}
+getch();
+}
