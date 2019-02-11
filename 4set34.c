d4d2a0d3bb5a972a890043335e4bfeb2b745e4ef
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:38:34 +0530

    Create 4set34.c

---
+++ 4set34.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+char str[50];
+int i,count=1;
+printf("enter the sentence:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]=='.')
+{
+count=count+1;
+}
+}
+printf("\nno of lines is=%d",count);
+getch();
+}
