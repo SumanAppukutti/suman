6fd98b588032de8deed481c01db1112b86716509
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:07:34 +0530

    Create 3setmini3.c

---
+++ 3setmini3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conoi.h>
+void main()
+{
+  int a[10],temp;
+  int i;
+  for(i=0;i<10;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  temp=a[0];
+  for(i=0;i<10;i++)
+  {
+    if(a[0]>a[i])
+    {
+      temp=a[i]);
+    }
+    printf("the largest number is %d",temp);
+  }
+}
+getch();
+}
