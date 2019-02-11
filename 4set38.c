8e1de2869fa000746754d8a27eed7dc822d19b2a
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:48:57 +0530

    Create 4set38.c

---
+++ 4set38.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void swap(int *a,int *b)
+{
+int temp=*a;
+*a=*b;
+*b=temp;
+}
+int main()
+{
+int a,b;
+printf("enter a:");
+scanf("%d",&a);
+printf("enter a:");
+scanf("%d",&b);
+swap(&a,&b);
+printf("after swapping a=%d and b=%d",a,yb
+getch();
+}
