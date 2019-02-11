808c97d54be79373ca233df0efb554ce1ae97813
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:35:25 +0530

    Create 4set33.c

---
+++ 4set33.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	char str[50];
+	int i,count=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nno of white spaces=%d",count);
+	getch();
+}
