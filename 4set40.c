b80e9aa033aea7be319307e425afe989caad1ace
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 14:52:53 +0530

    Create 4set40.c

---
+++ 4set40.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a,b,c,i;
+printf("enter the element");
+scanf("%d",&n);
+a=0;
+b=1;
+printf("%d%d",a,b);
+for(i=1;i<=n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+printf("%d",c);
+}
+getch();
+}
