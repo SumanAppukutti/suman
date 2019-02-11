b7726e6ead50ab1edcd391598153c3f0f3e507b1
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 16:22:51 +0530

    Create 8set78.c

---
+++ 8set78.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
