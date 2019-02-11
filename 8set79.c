9e36243dbdbba43fe4d71c75c97ad8744167ea94
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 16:26:27 +0530

    Create 8set79.c

---
+++ 8set79.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n1,n2,pro,i,flag=0;
+	printf("enter the two number:");
+	scanf("%d %d",&n1,&n2);
+	pro=n1*n2;
+	for(i=1;i<=pro/2;i++)
+	{
+		if(i*i==pro)
+		{
+			flag=1;
+			break;
+		}
+	
+	}
+	
+	if(flag==1)
+	{
+		printf("\nyes");
+	}
+	else
+	{
+		printf("\nno");
+	}
+	getch();
+}
