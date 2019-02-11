8a1a7d02173d0896e83cebeac8bdff42543f713e
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:14:56 +0530

    Create 5set50.c

---
+++ 5set50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf(" %d is power of 2",n);
+}
+else
+{
+printf("not a power of two");
+}
+getch();
+}
