451dd7c5d88232d85e9d340e65a14dc9c1c2fb00
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:50:16 +0530

    Create 7set65.c

---
+++ 7set65.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+getch();
+}
