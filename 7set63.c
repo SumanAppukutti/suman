cc8ead83af929d6bf2ae51d0f77449603b7fa6e9
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-11 15:46:17 +0530

    Create 7set63.c

---
+++ 7set63.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+char str[50];
+
+	int i,count=1;
+
+	printf("enter the sentence:");
+
+	scanf("%[^\n]s",str);
+
+	for(i=0;str[i]!='\0';i++)
+
+	{
+
+		if(str[i]==' ')
+
+		{
+
+			count=count+1;
+
+		}
+
+	}
+
+	printf("\nno of words=%d",count);
+
+getch();
+}
