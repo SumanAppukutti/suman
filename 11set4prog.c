7505b0b04254d2c4c350ff0a8647e8a020ece08c
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-18 08:08:17 -0800

    Create 11set4prog.c

---
+++ 11set4prog.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ 
+ int num1,num2,power;
+ printf("enter the number:");
+ scanf("%d\n%d\n",&num1,&num2);
+ power=pow(num1,num2);
+ printf("the power of given two number is %d",power);
+ getch();
+ getch();
+ }
