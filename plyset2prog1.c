3e3ed5c81218caac8e1287e746beaa9b611de2ae
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-19 07:17:13 -0800

    Create plyset2prog1.c

---
+++ plyset2prog1.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int i,j;
+    char a[100];
+    printf("enter the days");
+    scanf("%s",a);
+    if(a[0]=='s')
+        {
+            printf("holiday");
+        }
+        else
+        {
+            printf("working day");
+        }
+    getch();
+}
+    
