6f29cba7190e162626cbb61c5479134bebd0dea7
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-24 07:28:41 -0800

    Create plyset3prog25.c

---
+++ plyset3prog25.c
@@ -0,0 +1,16 @@
+#iclude<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+inti,n;
+int a[i];
+printf("\n enter the n number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter the array n element");
+scanf("%d",&n);
+}
+getch();
+}
