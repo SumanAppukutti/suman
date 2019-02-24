206f27703c4a71e20e87cb9f732bfa93499a1b3c
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-02-24 06:47:22 -0800

    Create plyset2prog20.c

---
+++ plyset2prog20.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+   int i,n,l;
+   char j,n[100];
+   printf("enter the str");
+   scanf("%[^\n]s",n);
+   for(i=0;n[i]!='\0';i++)
+   {
+       j=a[i];
+       l=j+3;
+       printf("%c",l);
+   }
+   getch();
+}
+   
+   
