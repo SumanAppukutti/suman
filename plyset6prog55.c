f7eb24f42ce85c4651014d64da56da6df1894819
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 08:30:56 -0700

    Create plyset6prog55.c

---
+++ plyset6prog55.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+int main(void) 
+{
+	char str1[20],str2[20];
+	int i,flag;
+	scanf("%s %s",str1,str2);
+	int l1,l2;
+	l1=strlen(str1);
+	l2=strlen(str2);
+	if(l1==l2)
+	{
+		for(i=0;i<l1;i++)
+		{
+			if(str1[i]=str2[i])
+			{
+				flag=0;
+			}
+			else
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1)
+		{
+			printf("no");
+		}
+		else
+		{
+			printf("yes");
+		}
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
