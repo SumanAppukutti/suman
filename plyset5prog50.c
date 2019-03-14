26e27befcc2f0a59505d752e8b4dba32c908be07
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 08:21:59 -0700

    Create plyset5prog50.c

---
+++ plyset5prog50.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int k,i;
+	scanf("%d",&k);
+	for(i=1;i<=100;i++)
+	if((i!=k)&&(i<k))
+	{
+	if(k%i==0)
+	{}
+	}
+	printf("yes");
+	return 0;
+}
