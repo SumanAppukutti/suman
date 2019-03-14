8b3d74b57d2b5ca94a01a47abc4349edc4f736c8
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 07:58:42 -0700

    Create plyset4prog40.c

---
+++ plyset4prog40.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int t1 = 0, t2 = 1, nextTerm = 0, n;
+printf("Enter a positive number: ");
+scanf("%d", &n);
+printf("Fibonacci Series: %d, %d, ", t1, t2);
+nextTerm = t1 + t2;
+while(nextTerm <= n)
+{
+printf("%d, ",nextTerm);
+t1 = t2;
+t2 = nextTerm;
+nextTerm = t1 + t2;
+}
+getch();
+}
