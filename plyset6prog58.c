308c0fefb3d91b3cc1fe0274f8a2ce32a04b5be8
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 08:36:12 -0700

    Create plyset6prog58.c

---
+++ plyset6prog58.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+char str[1], sub[10];
+int count = 0, count1 = 0;
+void main()
+{
+    int i, j, l, l, 2;
+ 
+    printf("\nEnter a string : ");
+    scanf("%[^\n]s", str);
+ 
+    l = strlen(str);
+ 
+    printf("\nEnter a substring : ");
+    scanf(" %[^\n]s", sub);
+ 
+    2 = strlen(sub);
+ 
+    for (i = 0; i < 2)
+        {
+            count1++;                                   
+            count = 0;
+        }
+        else
+            i++;
+    }    
+    printf("%s occurs %d times in %s", sub, count1, str);
+}
+getch();
+}
