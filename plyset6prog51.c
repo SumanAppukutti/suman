65411433a03d134291cc6062616d1998eec25638
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 08:23:23 -0700

    Create plyset6prog51.c

---
+++ plyset6prog51.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#includE<conio.h>
+int main()
+{
+    int n,i,j,a[10],temp=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[1]);
+   return 0;
+}
