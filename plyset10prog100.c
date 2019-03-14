190c4555a3143b01bdab7c3d52256c4a047a0da7
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-14 09:43:26 -0700

    Create plyset10prog100.c

---
+++ plyset10prog100.c
@@ -0,0 +1,18 @@
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	long int b;
+	int rem,sum,i=1;
+	scanf("%ld",&b);
+	while(b!=0)
+	{
+		rem=b%10;
+		sum=sum+rem*i;
+		i=i*2;
+		b=b/10;
+	}
+	printf("%lx",sum);
+}
