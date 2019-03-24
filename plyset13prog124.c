983f9364263156f1fd00c91eb4d37c7690b697f4
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-24 06:48:21 -0700

    Create plyset13prog124.c

---
+++ plyset13prog124.c
@@ -0,0 +1,38 @@
+
+
+#include <stdio.h>
+int main()
+{
+    int n,a[20],i,j,count=0,flag=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=2;i<1000;i++)
+	{
+	    count=0;
+	    for(j=0;j<n;j++)
+	    {
+	        if(i%a[j]==0)
+	        {
+	            count++;
+	        }
+	        else
+	        {
+	            break;
+	        }
+	    }
+	    if(count==n)
+	    {
+	        printf("%d is the common ",i);
+	        flag=1;
+	        break;
+	    }
+	}
+	if(flag==0)
+	{
+	    printf("no");
+	}
+	return 0;
+}
