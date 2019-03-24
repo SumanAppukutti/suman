7d5397099b05c03651ceeb9c13558e90f0f67ed1
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-24 10:19:44 -0700

    Create plyset17prog165.c

---
+++ plyset17prog165.c
@@ -0,0 +1,58 @@
+
+
+#include <stdio.h>
+int main(void)
+{
+	int n,k;
+	scanf("%d %d",&n,&k);
+	int a[n],i,j,temp,b,flag=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==k)
+		{
+			flag=1;
+			if(a[i+1==k])
+			{
+			b=a[i+2];
+			}
+			else
+			{
+				b=a[i+1];
+			}
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("%d",b);
+	}
+	else
+	{
+		for(i=0;i<n;i++)
+		{
+			if(a[i]>k)
+			{
+				printf("%d",a[i]);
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
