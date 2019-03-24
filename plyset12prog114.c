58120910c304f62ac0830544fa50d76230017dc3
SumanAppukutti <46133978+SumanAppukutti@users.noreply.github.com>
2019-03-24 06:33:03 -0700

    Create plyset12prog114.c

---
+++ plyset12prog114.c
@@ -0,0 +1,18 @@
+
+
+
+
+#include <stdio.h>
+int main() 
+{
+    int n,rem,sum=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+(rem*rem);
+        n=n/10;
+    }
+    printf("%d",sum);
+	return 0;
+}
